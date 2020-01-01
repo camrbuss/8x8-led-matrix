5f57202246541590f329a3a1ad390feef767b984
camrbuss <camrbuss@vt.edu>
2020-01-01 16:01:23 -0700

    C++ Formatted

--- Board.cpp
+++ Board.cpp
@@ -1,79 +1,93 @@
 #include "Board.h"
 
-Board::Board(uint8_t latchpin, uint8_t clockpin, uint8_t datapin) {
-		pinMode(latchpin, OUTPUT);
-		pinMode(clockpin, OUTPUT);
-		pinMode(datapin, OUTPUT);
-    this->_ltc = latchpin;
-    this->_clc = clockpin;
-    this->_dat = datapin;
-
+Board::Board(uint8_t latchpin, uint8_t clockpin, uint8_t datapin)
+{
+  pinMode(latchpin, OUTPUT);
+  pinMode(clockpin, OUTPUT);
+  pinMode(datapin, OUTPUT);
+  this->_ltc = latchpin;
+  this->_clc = clockpin;
+  this->_dat = datapin;
 }
 
-Board::~Board() {
+Board::~Board()
+{
   // Nothing
 }
 
-void Board::allOn() {
+void Board::allOn()
+{
   this->animation = false;
   memcpy(this->_frameCurrent, this->_frameOn, 8);
 }
 
-void Board::allOff() {
+void Board::allOff()
+{
   this->animation = false;
   memcpy(this->_frameCurrent, this->_frameOff, 8);
 }
 
-void Board::setHeart() {
+void Board::setHeart()
+{
   this->animation = false;
   memcpy(this->_frameCurrent, this->_frameHeart, 8);
 }
 
-void Board::setX() {
+void Board::setX()
+{
   this->animation = false;
   memcpy(this->_frameCurrent, this->_frameX, 8);
 }
 
-void Board::setSnake() {
+void Board::setSnake()
+{
   this->animation = true;
-  this->_numFrames = sizeof(_animSnake)/sizeof(_animSnake[0]);
-  memcpy(_animCurrent, _animSnake, sizeof(_animCurrent)); 
+  this->_numFrames = sizeof(_animSnake) / sizeof(_animSnake[0]);
+  memcpy(_animCurrent, _animSnake, sizeof(_animCurrent));
   this->_j = 0;
   this->setFrameRate(2);
-  
 }
 
-void Board::setSingleFrame(uint8_t frame[8]) {
+void Board::setSingleFrame(uint8_t frame[8])
+{
   this->animation = false;
   memcpy(this->_frameCurrent, frame, 8);
 }
 
-void Board::multiplexRow() {
+void Board::multiplexRow()
+{
   dataToSend = (0x01 << i) + (~_frameCurrent[i] << 8);
   digitalWrite(this->_ltc, LOW);
-  shiftOut(this->_dat, this->_clc, LSBFIRST, (dataToSend >> 8)); 
-  shiftOut(this->_dat, this->_clc, MSBFIRST, dataToSend); 
+  shiftOut(this->_dat, this->_clc, LSBFIRST, (dataToSend >> 8));
+  shiftOut(this->_dat, this->_clc, MSBFIRST, dataToSend);
   digitalWrite(this->_ltc, HIGH);
-  if (i == 7) {
-    i = 0;    
-  } else {
+  if (i == 7)
+  {
+    i = 0;
+  }
+  else
+  {
     i++;
   }
 }
 
 // Private Methods
 
-void Board::incrementFrame() {
-  if (_j < _numFrames - 1) {
+void Board::incrementFrame()
+{
+  if (_j < _numFrames - 1)
+  {
     memcpy(_frameCurrent, _animCurrent[_j], sizeof(_frameCurrent));
     _j++;
-  } else {
+  }
+  else
+  {
     memcpy(_frameCurrent, _animCurrent[_j], sizeof(_frameCurrent));
     _j = 0;
   }
 }
 
-void Board::setFrameRate(uint8_t frameRate) {
-  this->frameRateTimerMS = (uint16_t) (1000 / frameRate);
+void Board::setFrameRate(uint8_t frameRate)
+{
+  this->frameRateTimerMS = (uint16_t)(1000 / frameRate);
 }
-	
--- Board.h
+++ Board.h
@@ -4,19 +4,20 @@
 #include <Wire.h>
 #include <Arduino.h>
 
-extern "C" {
-  #include "user_interface.h" //For os_timer_t etc
+extern "C"
+{
+#include "user_interface.h" //For os_timer_t etc
 }
 
 class Board
 {
 public:
-	Board(uint8_t latchpin, uint8_t clockpin, uint8_t datapin);
-	~Board();
+  Board(uint8_t latchpin, uint8_t clockpin, uint8_t datapin);
+  ~Board();
 
   uint16_t frameRateTimerMS = 1000;
   bool animation = false;
- 
+
   void allOn();
   void allOff();
   void setHeart();
@@ -26,7 +27,7 @@ public:
   void setSingleFrame(uint8_t frame[8]);
   void setFrameRate(uint8_t frameRate);
   void incrementFrame();
-	
+
 private:
   uint8_t _frameCurrent[8] = {0, 0, 0, 0, 0, 0, 0, 0};
   uint8_t _frameHeart[8] = {0b01101100, 0b10010010, 0b10000010, 0b10000010, 0b01000100, 0b00101000, 0b00010000, 0x00};
@@ -44,9 +45,8 @@ private:
   uint8_t _ltc;
   uint8_t _clc;
   uint8_t _dat;
-  
-  uint8_t _numFrames = 0;
 
+  uint8_t _numFrames = 0;
 };
 
 #endif
